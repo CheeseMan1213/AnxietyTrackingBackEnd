8f2d624d01663e5c3cd3cbe1a9046b168d164fe3
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2019-10-09 21:25:54 -0500

    Removed some imports, made all member variables private and all of type String, and updated the getters and setters.

--- src/main/java/com/main/documents/AnxietyEntry.java
+++ src/main/java/com/main/documents/AnxietyEntry.java
@@ -1,50 +1,48 @@
 package com.main.documents;
-import com.main.enums.TodayWas;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.math.BigInteger;
-import java.util.Date;
 
 @Document(collection = "anxietyEntry")
 public class AnxietyEntry {
 
     //Member variables.
     @Id
-    private BigInteger _id;
-    Date _date;//Date that will be passed form the calendar page.
-    String _anxEntry;//The anxiety entry text.
-    TodayWas _todayWas;//For the radio buttons.
+    private String _id;
+    private String date;//Date that will be passed form the calendar page.
+    private String anxEntry;//The anxiety entry text.
+    private String todayWas;//For the radio buttons.
 
-    public BigInteger get_id() {
+
+
+    public String get_id() {
         return _id;
     }
 
-    public void set_id(BigInteger _id) {
+    public void set_id(String _id) {
         this._id = _id;
     }
 
-    public Date get_date() {
-        return _date;
+    public String getDate() {
+        return date;
     }
 
-    public void set_date(Date _date) {
-        this._date = _date;
+    public void setDate(String date) {
+        this.date = date;
     }
 
-    public String get_anxEntry() {
-        return _anxEntry;
+    public String getAnxEntry() {
+        return anxEntry;
     }
 
-    public void set_anxEntry(String _anxEntry) {
-        this._anxEntry = _anxEntry;
+    public void setAnxEntry(String anxEntry) {
+        this.anxEntry = anxEntry;
     }
 
-    public TodayWas get_todayWas() {
-        return _todayWas;
+    public String getTodayWas() {
+        return todayWas;
     }
 
-    public void set_todayWas(TodayWas _todayWas) {
-        this._todayWas = _todayWas;
+    public void setTodayWas(String todayWas) {
+        this.todayWas = todayWas;
     }
 }
