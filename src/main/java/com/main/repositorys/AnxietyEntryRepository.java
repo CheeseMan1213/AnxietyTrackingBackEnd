2431e4a9a092905aaef1ad87fc09f02a9a9fccf3
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2019-10-12 23:13:56 -0500

    Added abstract method findAllByDate()

--- src/main/java/com/main/repositorys/AnxietyEntryRepository.java
+++ src/main/java/com/main/repositorys/AnxietyEntryRepository.java
@@ -6,8 +6,9 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigInteger;
+import java.util.List;
 
 @Repository
 public interface AnxietyEntryRepository extends MongoRepository<AnxietyEntry, String> {
-    //
+    List<AnxietyEntry> findAllByDate(String date);
 }
